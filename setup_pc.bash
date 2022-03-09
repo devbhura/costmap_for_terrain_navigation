08f5129c87ef8d9146b46dadbaf4871e364cc46a
devbhura <devbhura@gmail.com>
2022-03-08 23:32:53 -0600

    remap

--- setup_pc.bash
+++ setup_pc.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 export ROS_MASTER_URI=http://jackal-desktop:11311
-export ROS_HOSTNAME= deveshh6
+export ROS_HOSTNAME=deveshh6
 source ~/Documents/winter_2022/indi_proj/ws/devel/setup.bash
